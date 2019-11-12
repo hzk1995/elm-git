eb545cdee11c4cf246bc5b65377ce6d0b8b49383
Ethan-1996 <2020249345@qq.com>
2019-11-12 16:47:41 +0800

    Ethan 下载axios 上传banner组件‘

---
+++ elm/elm/src/components/Banner.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="banner">
+        <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div 
+                class="swiper-slide"
+                v-for="banner in bannners"
+                :key="banner.id"
+            >
+                <img src="" alt="">
+            </div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination swiper-pagination-white"></div>
+        <!-- Add Arrows -->
+        <div class="swiper-button-next swiper-button-white"></div>
+        <div class="swiper-button-prev swiper-button-white"></div>
+    </div>
+
+</div>
+</template>
+
+<script>
+import Swiper from "swiper"
+export default {
+    data(){
+        return {
+            banners:[]
+        }
+    },
+    created(){
+        new Swiper(".banner",{
+            loop:true
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
