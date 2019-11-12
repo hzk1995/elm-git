8fb7ba551eaa56d2053b047eb5771be9455e8af9
hzk <819951125@qq.com>
2019-11-12 17:44:59 +0800

    111

---
+++ elm/elm/src/components/Tabbar/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="tabbar">
+		<Item
+			v-for="item in itemNavs"
+			:key="item.id"
+			:icon="item.icon"
+			:txt="item.txt"
+		>
+		</Item>
+	</div>
+</template>
+
+<script>
+	import Item from "./item.vue"
+	export default {
+		components:{
+			Item
+		},
+		data(){
+			return {
+				itemNavs:[
+					{id:1,icon:fa-ravelry,txt:"首页"},
+					{id:3,icon:fa-search-plus,txt:"发现"},
+					{id:3,icon:fa- file-text-o,txt:"订单"},
+					{id:4,icon:fa-user,txt:"我的"}
+				]
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ elm/elm/src/components/Tabbar/item.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="tabbar-item">
+		<i :class="['fa',icon]"></i>
+	</div>
+</template>
+
+<script>
+	export default {
+		props:["icon","txt"]
+	}
+</script>
+
+<style>
+</style>
